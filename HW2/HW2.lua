db2e6d48de0425c667408abe9f0ad3c0b1ab4c9d
Colton Gyulay <coltongyulay@gmail.com>
2016-02-15 01:59:10 -0500

    beginning torch implementation

--- HW2/HW2.lua
+++ HW2/HW2.lua
@@ -9,16 +9,31 @@ cmd = torch.CmdLine()
 cmd:option('-datafile', '', 'data file')
 cmd:option('-classifier', 'nb', 'classifier to use')
 
--- Hyperparameters
--- ...
-
-
 function main() 
    -- Parse input params
    opt = cmd:parse(arg)
+   classifier = opt.classifier
+   alpha = opt.alpha
+   lr = opt.lr
+   lambda = opt.lambda
+   n_epochs = opt.n_epochs
+   batch_size = opt.m
+
    local f = hdf5.open(opt.datafile, 'r')
+   nwords = f:read('words'):all():long()[1]
    nclasses = f:read('nclasses'):all():long()[1]
-   nfeatures = f:read('nfeatures'):all():long()[1]
+
+   -- Training, valid, and test windows
+   train_input_word_windows = f:read('train_input_word_windows'):all()
+   train_input_cap_windows = f:read('train_input_cap_windows'):all()
+   train_output = f:read('train_output'):all()
+
+   valid_input_word_windows = f:read('valid_input_word_windows'):all()
+   valid_input_cap_windows = f:read('valid_input_cap_windows'):all()
+   valid_output = f:read('valid_output'):all()
+
+   test_input_word_windows = f:read('test_input_word_windows'):all()
+   test_input_cap_windows = f:read('test_input_cap_windows'):all()
 
    local W = torch.DoubleTensor(nclasses, nfeatures)
    local b = torch.DoubleTensor(nclasses)
